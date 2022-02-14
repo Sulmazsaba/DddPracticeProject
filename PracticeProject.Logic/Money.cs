da959528230ee44cc80e941e582f88d5ca3644e3
Sulmaz <Sulmaz@DESKTOP-MM1IDAG>
2022-02-14 12:59:45 +0330

    complete money operations

--- PracticeProject.Logic/Money.cs
+++ PracticeProject.Logic/Money.cs
@@ -8,12 +8,19 @@ namespace PracticeProject.Logic
 {
     public sealed class Money : ValueObject<Money>
     {
-        public int OneCentCount { get; private set; }
-        public int TenCentCount { get; private set; }
-        public int QuarterCount { get; private set; }
-        public int OneDollorCount { get; private set; }
-        public int FiveDollorCount { get; private set; }
-        public int TwentyDollorCount { get; private set; }
+        public int OneCentCount { get; }
+        public int TenCentCount { get; }
+        public int QuarterCount { get; }
+        public int OneDollorCount { get; }
+        public int FiveDollorCount { get; }
+        public int TwentyDollorCount { get; }
+        public decimal Amount => OneCentCount * 0.01m +
+                      TenCentCount * 0.10m +
+                      QuarterCount * 0.25m +
+                      OneDollorCount +
+                      FiveDollorCount * 5 +
+                      TwentyDollorCount * 20;
+
 
         public Money(int oneCentCount, int tenCentCount, int quarterCount, int oneDollorCount, int fiveDollorCount, int twentyDollorCount)
         {
@@ -21,13 +28,13 @@ namespace PracticeProject.Logic
                 throw new InvalidOperationException();
             if (tenCentCount < 0)
                 throw new InvalidOperationException();
-            if(quarterCount < 0)
+            if (quarterCount < 0)
                 throw new InvalidOperationException();
-            if(fiveDollorCount < 0)
+            if (fiveDollorCount < 0)
                 throw new InvalidOperationException();
-            if(twentyDollorCount < 0)
+            if (twentyDollorCount < 0)
                 throw new InvalidOperationException();
-            if(oneDollorCount < 0)
+            if (oneDollorCount < 0)
                 throw new InvalidOperationException();
 
 
@@ -55,6 +62,16 @@ namespace PracticeProject.Logic
 
         }
 
+        public static Money operator -(Money money1, Money money2)
+        {
+            return new Money(money1.OneCentCount - money2.OneCentCount,
+                money1.TenCentCount - money2.TenCentCount,
+                money1.QuarterCount - money2.QuarterCount,
+                money1.OneDollorCount - money2.OneDollorCount,
+                money1.FiveDollorCount - money2.FiveDollorCount, money1.TwentyDollorCount - money2.TwentyDollorCount
+                );
+        }
+
         protected override bool EqualScore(Money other)
         {
             return other.OneCentCount == OneCentCount &&
@@ -70,11 +87,11 @@ namespace PracticeProject.Logic
             unchecked
             {
                 int hashCode = OneCentCount;
-                hashCode = (hashCode *397) ^ TenCentCount;
-                hashCode = (hashCode *397) ^ QuarterCount;
-                hashCode = (hashCode *397) ^ OneDollorCount;
-                hashCode = (hashCode *397) ^ FiveDollorCount;
-                hashCode = (hashCode *397) ^ TwentyDollorCount;
+                hashCode = (hashCode * 397) ^ TenCentCount;
+                hashCode = (hashCode * 397) ^ QuarterCount;
+                hashCode = (hashCode * 397) ^ OneDollorCount;
+                hashCode = (hashCode * 397) ^ FiveDollorCount;
+                hashCode = (hashCode * 397) ^ TwentyDollorCount;
                 return hashCode;
             }
         }
--- PracticeProject.Tests/MoneySpecs.cs
+++ PracticeProject.Tests/MoneySpecs.cs
@@ -49,7 +49,7 @@ namespace PracticeProject.Tests
         [InlineData(0, 0, -1, 0, 0, 0)]
         [InlineData(0, 0, 0, -1, 0, 0)]
         [InlineData(0, 0, 0, 0, -1, 0)]
-        public void cannot_create_money_with_negative_value(int oneCentCount,
+        public void Cannot_create_money_with_negative_value(int oneCentCount,
             int tenCentCount,
             int quarterCount,
             int oneDollorCount,
@@ -59,6 +59,62 @@ namespace PracticeProject.Tests
             Action action = () => new Money(oneCentCount,
                 tenCentCount, quarterCount, oneDollorCount,
                 fiveDollorCount, twentyDollorCount);
+
+            action.Should().Throw<InvalidOperationException>();
+        }
+
+
+        [Theory]
+        [InlineData(0, 0, 0, 0, 0, 0, 0)]
+        [InlineData(1, 0, 0, 0, 0, 0, 0.01)]
+        [InlineData(1, 2, 0, 0, 0, 0, 0.21)]
+        [InlineData(1, 2, 3, 0, 0, 0, 0.96)]
+        [InlineData(1, 2, 3, 4, 0, 0, 4.96)]
+        [InlineData(1, 2, 3, 4, 5, 0, 29.96)]
+        [InlineData(1, 2, 3, 4, 5, 6, 149.96)]
+        [InlineData(110, 0, 0, 0, 100, 0, 501.1)]
+        public void Amount_is_calculated_correctly(int oneCentCount,
+            int tenCentCount,
+            int quarterCount,
+            int oneDollorCount,
+            int fiveDollorCount,
+            int twentyDollorCount,
+            decimal expectedAmount
+            )
+        {
+
+            var money = new Money(oneCentCount, tenCentCount, quarterCount, oneDollorCount, fiveDollorCount, twentyDollorCount);
+            money.Amount.Should().Be(expectedAmount);
+
+        }
+
+        [Fact]
+        public void Substraction_of_two_moneys_produces_correct_result()
+        {
+
+            Money money1 = new Money(10, 10, 10, 10, 10, 10);
+            Money money2 = new Money(1, 2, 3, 4, 5, 6);
+
+            Money result = money1 - money2;
+
+            result.OneCentCount.Should().Be(9);
+            result.TenCentCount.Should().Be(8);
+            result.QuarterCount.Should().Be(7);
+            result.OneDollorCount.Should().Be(6);
+            result.FiveDollorCount.Should().Be(5);
+            result.TwentyDollorCount.Should().Be(4);
+        }
+
+        [Fact]
+        public void Cannot_substract_more_than_exist()
+        {
+            Money money1 = new Money(0, 1, 0, 0, 0, 0);
+            Money money2 = new Money(1, 0, 0, 0, 0, 0);
+
+            var action = () =>
+            {
+                Money result = money1 - money2;
+            };
 
             action.Should().Throw<InvalidOperationException>();
         }
